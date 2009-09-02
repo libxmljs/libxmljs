80a7b096af82a568ac4fb4be312661eef47586b9
Jeff Smick <sprsquish@gmail.com>
2009-09-02 16:33:17 -0700

    reorg

--- samples/sax.js
+++ samples/sax.js
@@ -1,9 +1,12 @@
-var parser = libxml.sax.pushParser({
+var callbacks = {
   document: null,
   current: null,
 
   startDocument: function() {},
   endDocument: function() {},
+
+  startElement: function(elem, attrs) {},
+  endElement: function(elem) {},
 
   startElementNS: function(elem, attrs, prefix, uri, namespaces) {},
   endElementNS: function(elem, prefix, uri) {},
@@ -13,8 +16,17 @@ var parser = libxml.sax.pushParser({
 
   warning: function(msg) {},
   error: function(msg) {}
-});
+};
+
+
+/** SAX Push Parser **/
+var saxPushParser = libxml.sax.createPushParser(callbacks);
 
 while(data) {
-  parser.push(data);
+  saxPushParser.push(data);
 };
+
+
+/** SAX Parser **/
+var saxParser = libxml.sax.createParser(callbacks);
+saxParser.parse(document);
