5e9ba6ef91d5e1002fb6a4d2a70b320271c8391a
Jeff Smick <sprsquish@gmail.com>
2009-09-20 09:31:41 -0700

    strip out more node.js specifics

--- src/object_wrap.h
+++ src/object_wrap.h
@@ -38,28 +38,9 @@ public:
     assert(handle->InternalFieldCount() > 0);
     handle_ = v8::Persistent<v8::Object>::New(handle);
     handle_->SetInternalField(0, v8::External::New(this));
-    MakeWeak();
-  }
-
-  inline void
-  MakeWeak (
-    void)
-  {
-    handle_.MakeWeak(this, WeakCallback);
   }
 
   v8::Persistent<v8::Object> handle_; // ro
-
-private:
-  static void
-  WeakCallback(
-    v8::Persistent<v8::Value> value,
-    void *data)
-  {
-    ObjectWrap *obj = static_cast<ObjectWrap*>(data);
-    assert(value == obj->handle_);
-    obj->MakeWeak();
-  }
 };
 
 } // namespace node
