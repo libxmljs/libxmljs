1b920d5a1521b5bdf8560a428aaf4144d6be83cc
Jeff Smick <sprsquish@gmail.com>
2009-10-03 23:17:43 -0700

    apply cpplint to parser.cc and parser.h

--- src/parser.cc
+++ src/parser.cc
@@ -1,48 +1,45 @@
+// Copyright 2009, Squish Tech, LLC.
 #include "parser.h"
+
 #include "sax_parser.h"
 
-using namespace v8;
-using namespace libxmljs;
+namespace libxmljs {
 
 v8::Handle<v8::Value>
-ParseString(
-  const v8::Arguments& args)
-{
-  HandleScope scope;
+ParseString(const v8::Arguments& args) {
+  v8::HandleScope scope;
 
   if (!args[0]->IsString())
-    return ThrowException(Exception::Error(String::New("Must supply parseString with a string")));
+    return v8::ThrowException(v8::Exception::Error(
+      v8::String::New("Must supply parseString with a string")));
 
   Parser * parser = new Parser();
-  String::Utf8Value str(args[0]->ToString());
+  v8::String::Utf8Value str(args[0]->ToString());
   return parser->parse_string(*str, str.length());
 }
 
 v8::Handle<v8::Value>
-ParseFile(
-  const v8::Arguments& args)
-{
-  HandleScope scope;
+ParseFile(const v8::Arguments& args) {
+  v8::HandleScope scope;
 
   if (!args[0]->IsString())
-    return ThrowException(Exception::Error(String::New("Must supply parseFile with a filename")));
+    return v8::ThrowException(v8::Exception::Error(
+      v8::String::New("Must supply parseFile with a filename")));
 
   Parser * parser = new Parser();
-  String::Utf8Value str(args[0]->ToString());
+  v8::String::Utf8Value str(args[0]->ToString());
   return parser->parse_file(*str);
 }
 
 void
-Parser::initializeContext()
-{
+Parser::initializeContext() {
   assert(context_);
   context_->validate = 0;
   context_->_private = this;
 }
 
 void
-Parser::releaseContext()
-{
+Parser::releaseContext() {
   if (context_) {
     context_->_private = 0;
     if (context_->myDoc != NULL)
@@ -53,78 +50,76 @@ Parser::releaseContext()
   }
 }
 
-Handle<Value>
-Parser::parse_file(
-  const char * filename)
-{
-  releaseContext(); //Free any existing document.
+v8::Handle<v8::Value>
+Parser::parse_file(const char * filename) {
+  releaseContext();  // Free any existing document.
 
-  //The following is based on the implementation of xmlParseFile(), in xmlSAXParseFileWithData():
+  // The following is based on the implementation of
+  // xmlParseFile(), in xmlSAXParseFileWithData():
   context_ = xmlCreateFileParserCtxt(filename);
 
-  if(context_->directory == 0) {
-    char *directory = xmlParserGetDirectory(filename);
-    context_->directory = (char *)xmlStrdup((xmlChar*) directory);
+  if (context_->directory == 0) {
+    xmlChar* directory = reinterpret_cast<xmlChar*>(xmlParserGetDirectory(filename));
+    context_->directory = reinterpret_cast<char *>(xmlStrdup(directory));
   }
 
   return parse_context();
 }
 
-Handle<Value>
-Parser::parse_string(
-  const char * str,
-  int len)
-{
-  releaseContext(); //Free any existing document.
+v8::Handle<v8::Value>
+Parser::parse_string(const char * str, int len) {
+  releaseContext();  // Free any existing document.
 
-  //The following is based on the implementation of xmlParseFile(), in xmlSAXParseFileWithData():
+  // The following is based on the implementation of
+  // xmlParseFile(), in xmlSAXParseFileWithData():
   context_ = xmlCreateMemoryParserCtxt(str, len);
   return parse_context();
 }
 
-Handle<Value>
-Parser::parse_context()
-{
-  //The following is based on the implementation of xmlParseFile(), in xmlSAXParseFileWithData():
-  //and the implementation of xmlParseMemory(), in xmlSaxParseMemoryWithData().
+v8::Handle<v8::Value>
+Parser::parse_context() {
+  // The following is based on the implementation of
+  // xmlParseFile(), in xmlSAXParseFileWithData():
+  // and the implementation of xmlParseMemory(), in
+  // xmlSaxParseMemoryWithData().
   initializeContext();
 
-  if(!context_)
-    return Null();
+  if (!context_)
+    return v8::Null();
 
   xmlParseDocument(context_);
 
-  if(!context_->wellFormed) {
-    releaseContext(); //Free doc_;
-    return Null();
+  if (!context_->wellFormed) {
+    releaseContext();  // Free doc_;
+    return v8::Null();
   }
 
-  if(context_->errNo != 0) {
+  if (context_->errNo != 0) {
     releaseContext();
-    return Null();
+    return v8::Null();
   }
 
-  Persistent<Object> doc = XmlObj::Unwrap(context_->myDoc);
+  v8::Persistent<v8::Object> doc = XmlObj::Unwrap(context_->myDoc);
 
   // This is to indicate to release_underlying that we took the
   // ownership on the doc.
   context_->myDoc = NULL;
 
-  //Free the parse context, but keep the document alive so people can navigate the DOM tree:
-  //TODO: Why not keep the context alive too?
+  // Free the parse context, but keep the document alive
+  // so people can navigate the DOM tree:
+  // TODO(sprsquish): Why not keep the context alive too?
   releaseContext();
 
   return doc;
 }
 
 void
-Parser::Initialize (
-  Handle<Object> target)
-{
-  HandleScope scope;
+Parser::Initialize(v8::Handle<v8::Object> target) {
+  v8::HandleScope scope;
 
   LIBXMLJS_SET_METHOD(target, "parseString", ParseString);
   LIBXMLJS_SET_METHOD(target, "parseFile", ParseFile);
 
   SaxParser::Initialize(target);
 }
+}  // namespace libxmljs
--- src/parser.h
+++ src/parser.h
@@ -1,26 +1,30 @@
-#ifndef __parser_h__
-#define __parser_h__
+// Copyright 2009, Squish Tech, LLC.
+#ifndef SRC_PARSER_H_
+#define SRC_PARSER_H_
+
+#include <v8.h>
+
+#include <libxml/parser.h>
+#include <libxml/parserInternals.h>  // for xmlCreateFileParserCtxt
+
+#include <cassert>  // for assert()
 
 #include "libxmljs.h"
 #include "object_wrap.h"
 
-#include <v8.h>
-#include <cassert> // for assert()
-
-#include <libxml/parser.h>
-#include <libxml/parserInternals.h> // for xmlCreateFileParserCtxt
-
 namespace libxmljs {
 
 class Parser : public ObjectWrap {
-public:
-  Parser() : context_(0) {};
-  static void Initialize (v8::Handle<v8::Object> target);
+  public:
+
+  Parser() : context_(0) {}
+  static void Initialize(v8::Handle<v8::Object> target);
 
   v8::Handle<v8::Value> parse_string(const char * str, int len);
   v8::Handle<v8::Value> parse_file(const char * filename);
 
-protected:
+  protected:
+
   v8::Handle<v8::Value> parse_context();
 
   virtual void initializeContext();
@@ -29,6 +33,6 @@ protected:
   _xmlParserCtxt* context_;
 };
 
-} // namespace libxmljs
+}  // namespace libxmljs
 
-#endif //__parser_h__
+#endif  // SRC_PARSER_H_
