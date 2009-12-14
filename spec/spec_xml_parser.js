faba1e8b4a4598f68a6833c589ef63de461d539d
Jeff Smick <sprsquish@gmail.com>
2009-12-14 07:58:09 -0800

    distinguish between xml and html parsers in spec filenames

--- spec/spec_xml_parser.js
+++ spec/spec_xml_parser.js
@@ -30,23 +30,3 @@ describe('Parsing XML', function() {
     assertEqual('with content!', doc.get('sibling').text());
   });
 });
-
-describe('Parsing HTML', function() {
-  var filename = path.dirname(__filename)+'/fixtures/html_parser_test.html';
-
-  it('can be done by string', function() {
-    var str = posix.cat(filename).wait();
-  
-    var doc = libxml.parseHtmlString(str);
-    assertEqual('html', doc.root().name());
-    assertEqual('Test HTML document', doc.get('head/title').text());
-    assertEqual('HTML content!', doc.get('body/span').text());
-  });
-
-  it('can be done by file', function() {
-    var doc = libxml.parseHtmlFile(filename);
-    assertEqual('html', doc.root().name());
-    assertEqual('Test HTML document', doc.get('head/title').text());
-    assertEqual('HTML content!', doc.get('body/span').text());
-  });
-});
