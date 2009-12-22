b21d7713bcf7847b73e27e76ef3a55becccaa135
Jeff Smick <sprsquish@gmail.com>
2009-12-21 19:10:27 -0800

    Forgot to add the text node spec

---
+++ spec/spec_text_node.js
@@ -0,0 +1,11 @@
+process.mixin(require('./helpers'));
+
+describe('Text node', function() {
+  it('knows its type is "text"', function() {
+    var doc = libxml.parseXmlString(
+      '<?xml version="1.0"?>\
+      <root>child</root>\
+    ');
+    assertEqual('text', doc.child().type());
+  });
+});
