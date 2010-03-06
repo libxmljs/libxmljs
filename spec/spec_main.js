cf1c38c0a805364943163cef6bf8176276dc5944
Jeff Smick <sprsquish@gmail.com>
2010-03-05 20:07:41 -0800

    minor formatting

--- spec/spec_main.js
+++ spec/spec_main.js
@@ -2,17 +2,17 @@ process.mixin(require('./helpers'));
 
 describe('libxmljs', function() {
   it('has a version number', function() {
-    if (specVerbose) print(libxml.version);
+    if (specVerbose) print(libxml.version+" ");
     assert(typeof libxml.version == 'string');
   });
 
   it('knows the libxml version number', function() {
-    if (specVerbose) print(libxml.libxml_version);
+    if (specVerbose) print(libxml.libxml_version+" ");
     assert(typeof libxml.libxml_version == 'string');
   });
 
   it('knows the libxml parser version number', function() {
-    if (specVerbose) print(libxml.libxml_parser_version);
+    if (specVerbose) print(libxml.libxml_parser_version+" ");
     assert(typeof libxml.libxml_parser_version == 'string');
   });
 });
