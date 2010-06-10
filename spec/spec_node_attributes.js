2ab6e54a091e8fc1eeffb1cee2e62a387325ef11
Marco Rogers <marco.rogers@gmail.com>
2010-06-10 00:30:08 -0400

    updated test to check elements with no attributes

--- spec/spec_node_attributes.js
+++ spec/spec_node_attributes.js
@@ -8,26 +8,26 @@ describe('A node attribute', function() {
     new libxml.Attribute(elem, 'to', 'wongfoo');
     assertEqual('wongfoo', elem.attr('to').value());
   });
-  
+
   it('will return null when an attr is not found', function() {
     var doc = new libxml.Document();
     elem = doc.node('name');
     assertEqual(null, elem.attr('to'));
   });
-  
+
   it('can be assigned on creation', function() {
     var doc = new libxml.Document();
     var elem = doc.node('name', {to: 'wongfoo'});
     assertEqual('wongfoo', elem.attr('to').value());
   });
-  
+
   it('can be assigned with an object', function() {
     var doc = new libxml.Document();
     elem = doc.node('name');
     elem.attr({to: 'wongfoo'});
     assertEqual('wongfoo', elem.attr('to').value());
   });
-  
+
   it('can be reassigned', function() {
     var doc = new libxml.Document();
     var elem = doc.node('name', {to: 'wongfoo'});
@@ -35,20 +35,23 @@ describe('A node attribute', function() {
     elem.attr({to: 'julie newmar'});
     assertEqual('julie newmar', elem.attr('to').value());
   });
-  
+
   it('can be retrieved in an array', function() {
     var doc = new libxml.Document();
-    var elem = doc.node('root')
-                  .attr({foo: 'bar'})
-                  .attr({bar: 'baz'})
-                  .attr({baz: 'foo'});
-  
+    var elem = doc.node('root');
+
+    assertEqual([], elem.attrs());
+
+    elem.attr({foo: 'bar'})
+        .attr({bar: 'baz'})
+        .attr({baz: 'foo'});
+
     var attrs = [elem.attr('foo'), elem.attr('bar'), elem.attr('baz')];
     var i;
     for (i = 0; i < 3; i++)
       assertEqual(attrs[i], elem.attrs()[i]);
   });
-  
+
   it('can traverse over its siblings', function() {
     var doc = new libxml.Document();
     var elem = doc.node('root')
@@ -60,14 +63,14 @@ describe('A node attribute', function() {
     assertEqual(null, elem.attr('foo').prevSibling());
     assertEqual(null, elem.attr('baz').nextSibling());
   });
-  
+
   it('can get back to its node', function() {
     var doc = new libxml.Document();
     var elem = doc.node('root')
                   .attr({foo: 'bar'});
     assertEqual(elem, elem.attr('foo').parent());
   });
-  
+
   it('can get back to its document', function() {
     var doc = new libxml.Document();
     var elem = doc.node('root')
