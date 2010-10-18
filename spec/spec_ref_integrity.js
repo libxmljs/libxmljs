920005a12267a4d7736132eba0470efd1bd25997
Marco Rogers <marco.rogers@gmail.com>
2010-10-18 01:50:19 -0400

    removed error logging from tests

--- spec/spec_ref_integrity.js
+++ spec/spec_ref_integrity.js
@@ -4,16 +4,12 @@ describe('Referential Integrity', function() {
 
     it("doesn't crash", function() {
         var doc = new libxml.Document();
-        console.error(0);
         doc.node('root').node('child').node('grandchild').parent().node('child2');
         gc();
-        console.error(1);
         assert(doc, "doc");
         gc();
-        console.error(2);
         assert(doc.root(), "root");
         gc();
-        console.error(3);
         assertEqual("child", doc.root().childNodes()[0].name(), "child name");
     });
 
