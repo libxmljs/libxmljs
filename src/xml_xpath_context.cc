eb7becf771919cf0e82a0753bd4591beb842810d
Marco Rogers <marco.rogers@gmail.com>
2011-12-16 01:40:46 -0800

    remove unnecessary array push func

--- src/xml_xpath_context.cc
+++ src/xml_xpath_context.cc
@@ -35,8 +35,6 @@ XmlXpathContext::evaluate(const xmlChar* xpath) {
   }
 
   v8::Handle<v8::Array> nodes = v8::Array::New(result->nodesetval->nodeNr);
-  v8::Handle<v8::Function> push = v8::Handle<v8::Function>::Cast(
-    nodes->Get(v8::String::NewSymbol("push")));
   for (int i = 0; i != result->nodesetval->nodeNr; ++i) {
     xmlNode *node = result->nodesetval->nodeTab[i];
     nodes->Set(i,LibXmlObj::GetMaybeBuild<XmlElement, xmlNode>(node));
