72464eb70a78d8dd912f21b5a41400aee618e930
Marco Rogers <marco.rogers@gmail.com>
2012-09-09 22:09:11 -0700

    remove v from intrenal version, and test that it always matches package version

--- test/main.js
+++ test/main.js
@@ -1,7 +1,9 @@
-var libxml = require('../index');
+var package = require('../package')
+  , libxml = require('../index');
 
 module.exports.constants = function(assert) {
     assert.ok(typeof libxml.version == 'string');
+    assert.equal(package.version, libxml.version);
     assert.ok(typeof libxml.libxml_version == 'string');
     assert.ok(typeof libxml.libxml_parser_version == 'string');
     assert.ok(typeof libxml.libxml_debug_enabled == 'boolean');
