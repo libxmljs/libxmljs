f621f51b34f8bd823c60d726bcd52a6385019953
Roman Shtylman <shtylman@gmail.com>
2013-05-09 11:30:13 -0400

    comment out HAVE_ZLIB_H
    we don't need zlib support
    
    see #195

--- vendor/libxml/config.h
+++ vendor/libxml/config.h
@@ -261,7 +261,7 @@
 #define HAVE_VSPRINTF 1
 
 /* Define to 1 if you have the <zlib.h> header file. */
-#define HAVE_ZLIB_H 1
+/* #define HAVE_ZLIB_H 1 */
 
 /* Define to 1 if you have the `_stat' function. */
 /* #undef HAVE__STAT */
