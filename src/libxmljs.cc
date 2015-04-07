97f2135a760cea715bf204685e11e6550b15a262
Martin von Gagern <Martin.vGagern@gmx.net>
2015-04-07 13:20:34 +0200

    Implement thread-local memory adjustments.
    If the current thread is not the main V8 event thread, we don't report
    memory adjustments back to V8 immediately, but instead keep track of all the
    changes in a thread-local variable.  The net change will be reported back
    after the worker is done.
    
    All state changes are encapsulated in two RAII sentinels: WorkerParent and
    WorkerSentinel.  The former is constructed and destroyed on the V8 thread,
    the latter on the worker thread.  Having these in place helps to make client
    code thread-safe with respect to the state in question.  It also provides a
    convenient extension point: if, at some point, we need more code for
    handling worker threads, all we have to do is extend these two classes.
    
    The current delegation to the libxml2 memory management functions has ben
    completely dropped.  The main use case for these would have been memory
    debugging, so they do a lot of work (like maintaining linked lists between
    allocated blocks) that we don't need.  The main thing we could have gained
    from them, namely the call to xmlMemUsed, was not thread-safe in any case.

--- src/libxmljs.cc
+++ src/libxmljs.cc
@@ -11,37 +11,71 @@
 
 namespace libxmljs {
 
-// ensure destruction at exit time
-// v8 doesn't cleanup its resources
-LibXMLJS LibXMLJS::instance;
+bool tlsInitialized = false;
+uv_key_t tlsKey;
+bool isAsync = false; // Only set on V8 thread when no workers are running
+int numWorkers = 0; // Only access from V8 thread
 
-// track how much memory libxml2 is using
-int xml_memory_used = 0;
-
-// wrapper for xmlMemMalloc to update v8's knowledge of memory used
-// the GC relies on this information
-void* xmlMemMallocWrap(size_t size)
-{
-    void* res = xmlMemMalloc(size);
-
-    // no need to udpate memory if we didn't allocate
-    if (!res)
+// Set up in V8 thread
+WorkerParent::WorkerParent() : memAdjustments(0) {
+    if (!tlsInitialized)
     {
-        return res;
+        uv_key_create(&tlsKey);
     }
-
-    const int diff = xmlMemUsed() - xml_memory_used;
-    xml_memory_used += diff;
-    NanAdjustExternalMemory(diff);
-    return res;
+    if (numWorkers++ == 0)
+    {
+        isAsync = true;
+    }
 }
 
-// wrapper for xmlMemFree to update v8's knowledge of memory used
-// the GC relies on this information
-void xmlMemFreeWrap(void* p)
-{
-    xmlMemFree(p);
+// Tear down in V8 thread
+WorkerParent::~WorkerParent() {
+    NanAdjustExternalMemory(memAdjustments);
+    if (--numWorkers == 0)
+    {
+        isAsync = false;
+    }
+}
 
+// Set up in worker thread
+WorkerSentinel::WorkerSentinel(WorkerParent& parent) : parent(parent) {
+    uv_key_set(&tlsKey, this);
+}
+
+// Tear down in worker thread
+WorkerSentinel::~WorkerSentinel() {
+    uv_key_set(&tlsKey, NULL);
+}
+
+struct memHdr {
+    size_t size;
+};
+
+// When we move to C++11 one day, the following would become constexpr,
+// and SLIGN_SIZE could perhaps become sizeof(std::max_align_t).
+#define ALIGN_SIZE sizeof(double)
+#define HDR_SIZE (((sizeof(memHdr) + (ALIGN_SIZE - 1))/ALIGN_SIZE)*ALIGN_SIZE)
+
+inline void* hdr2client(memHdr* hdr) {
+    return static_cast<void*>(reinterpret_cast<char*>(hdr) + HDR_SIZE);
+}
+
+inline memHdr* client2hdr(void* client) {
+    return reinterpret_cast<memHdr*>(static_cast<char*>(client) - HDR_SIZE);
+}
+
+void adjustMem(ssize_t diff)
+{
+    if (isAsync)
+    {
+        WorkerSentinel* worker =
+            static_cast<WorkerSentinel*>(uv_key_get(&tlsKey));
+        if (worker)
+        {
+            worker->parent.memAdjustments += diff;
+            return;
+        }
+    }
     // if v8 is no longer running, don't try to adjust memory
     // this happens when the v8 vm is shutdown and the program is exiting
     // our cleanup routines for libxml will be called (freeing memory)
@@ -57,59 +91,57 @@ void xmlMemFreeWrap(void* p)
     {
         return;
     }
-
-    const int diff = xmlMemUsed() - xml_memory_used;
-    xml_memory_used += diff;
     NanAdjustExternalMemory(diff);
 }
 
-// wrapper for xmlMemRealloc to update v8's knowledge of memory used
-void* xmlMemReallocWrap(void* ptr, size_t size)
+void* memMalloc(size_t size)
 {
-    void* res = xmlMemRealloc(ptr, size);
+    size_t totalSize = size + HDR_SIZE;
+    memHdr* mem = static_cast<memHdr*>(malloc(totalSize));
+    if (!mem) return NULL;
+    mem->size = size;
+    adjustMem(totalSize);
+    return hdr2client(mem);
+}
 
-    // if realloc fails, no need to update v8 memory state
-    if (!res)
-    {
-        return res;
-    }
+void memFree(void* p)
+{
+    memHdr* mem = client2hdr(p);
+    ssize_t totalSize = mem->size + HDR_SIZE;
+    adjustMem(-totalSize);
+    free(mem);
+}
 
-    const int diff = xmlMemUsed() - xml_memory_used;
-    xml_memory_used += diff;
-    NanAdjustExternalMemory(diff);
+void* memRealloc(void* ptr, size_t size)
+{
+    memHdr* mem1 = client2hdr(ptr);
+    ssize_t oldSize = mem1->size;
+    memHdr* mem2 = static_cast<memHdr*>(realloc(mem1, size + HDR_SIZE));
+    if (!mem2) return NULL;
+    mem2->size = size;
+    adjustMem(ssize_t(size) - oldSize);
+    return hdr2client(mem2);
+}
+
+char* memStrdup(const char* str)
+{
+    size_t size = strlen(str) + 1;
+    char* res = static_cast<char*>(memMalloc(size));
+    if (res) strcpy(res, str);
     return res;
 }
 
-// wrapper for xmlMemoryStrdupWrap to update v8's knowledge of memory used
-char* xmlMemoryStrdupWrap(const char* str)
-{
-    char* res = xmlMemoryStrdup(str);
-
-    // if strdup fails, no need to update v8 memory state
-    if (!res)
-    {
-        return res;
-    }
-
-    const int diff = xmlMemUsed() - xml_memory_used;
-    xml_memory_used += diff;
-    NanAdjustExternalMemory(diff);
-    return res;
-}
+// ensure destruction at exit time
+// v8 doesn't cleanup its resources
+LibXMLJS LibXMLJS::instance;
 
 LibXMLJS::LibXMLJS()
 {
-
-    // populated debugMemSize (see xmlmemory.h/c) and makes the call to
-    // xmlMemUsed work, this must happen first!
-    xmlMemSetup(xmlMemFreeWrap, xmlMemMallocWrap,
-            xmlMemReallocWrap, xmlMemoryStrdupWrap);
+    // Setup our own memory handling (see xmlmemory.h/c)
+    xmlMemSetup(memFree, memMalloc, memRealloc, memStrdup);
 
     // initialize libxml
     LIBXML_TEST_VERSION;
-
-    // initial memory usage
-    xml_memory_used = xmlMemUsed();
 }
 
 LibXMLJS::~LibXMLJS()
