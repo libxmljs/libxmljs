209342e10f67782b2d2f8b5d0d1bcc082bed986a
Ashkan Daie <Ashkan Daie>
2016-02-01 23:28:15 -0800

    Added preprocessor conditions to config.h to build on windows platforms.

--- vendor/libxml/config.h
+++ vendor/libxml/config.h
@@ -157,7 +157,9 @@
 #define HAVE_RAND 1
 
 /* Define to 1 if you have the `rand_r' function. */
+#ifndef WIN32
 #define HAVE_RAND_R 1
+#endif
 
 /* Define to 1 if you have the <resolv.h> header file. */
 #define HAVE_RESOLV_H 1
@@ -249,7 +251,9 @@
 #define HAVE_TIME_H 1
 
 /* Define to 1 if you have the <unistd.h> header file. */
+#ifndef WIN32
 #define HAVE_UNISTD_H 1
+#endif
 
 /* Whether va_copy() is available */
 #define HAVE_VA_COPY 1
