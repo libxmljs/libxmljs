bd3d0b46d414bc5047a435fee250fd542210dad3
marudor <marudor@marudor.de>
2019-05-21 22:51:30 +0200

    Update libxml2 to 2.9.9

--- vendor/libxml/config.h
+++ vendor/libxml/config.h
@@ -4,9 +4,6 @@
 /* Type cast for the gethostbyname() argument */
 #define GETHOSTBYNAME_ARG_CAST /**/
 
-/* Define to 1 if you have the <ansidecl.h> header file. */
-/* #undef HAVE_ANSIDECL_H */
-
 /* Define to 1 if you have the <arpa/inet.h> header file. */
 #define HAVE_ARPA_INET_H 1
 
@@ -15,9 +12,6 @@
 
 /* Whether struct sockaddr::__ss_family exists */
 /* #undef HAVE_BROKEN_SS_FAMILY */
-
-/* Define to 1 if you have the `class' function. */
-/* #undef HAVE_CLASS */
 
 /* Define to 1 if you have the <ctype.h> header file. */
 #define HAVE_CTYPE_H 1
@@ -40,23 +34,11 @@
 /* Define to 1 if you have the <fcntl.h> header file. */
 #define HAVE_FCNTL_H 1
 
-/* Define to 1 if you have the `finite' function. */
-#define HAVE_FINITE 1
-
 /* Define to 1 if you have the <float.h> header file. */
 #define HAVE_FLOAT_H 1
 
-/* Define to 1 if you have the `fpclass' function. */
-/* #undef HAVE_FPCLASS */
-
 /* Define to 1 if you have the `fprintf' function. */
 #define HAVE_FPRINTF 1
-
-/* Define to 1 if you have the `fp_class' function. */
-/* #undef HAVE_FP_CLASS */
-
-/* Define to 1 if you have the <fp_class.h> header file. */
-/* #undef HAVE_FP_CLASS_H */
 
 /* Define to 1 if you have the `ftime' function. */
 #define HAVE_FTIME 1
@@ -66,9 +48,6 @@
 
 /* Define to 1 if you have the `gettimeofday' function. */
 #define HAVE_GETTIMEOFDAY 1
-
-/* Define to 1 if you have the <ieeefp.h> header file. */
-/* #undef HAVE_IEEEFP_H */
 
 /* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
@@ -82,23 +61,14 @@
 /* Define if isnan is there */
 #define HAVE_ISNAN /**/
 
-/* Define to 1 if you have the `isnand' function. */
-/* #undef HAVE_ISNAND */
-
 /* Define if history library is there (-lhistory) */
 /* #undef HAVE_LIBHISTORY */
 
-/* Have compression library */
-/* #define HAVE_LIBLZMA 1 */
-
 /* Define if pthread library is there (-lpthread) */
-/* #define HAVE_LIBPTHREAD */
+#define HAVE_LIBPTHREAD /**/
 
 /* Define if readline library is there (-lreadline) */
 /* #undef HAVE_LIBREADLINE */
-
-/* Have compression library */
-#define HAVE_LIBZ 1
 
 /* Define to 1 if you have the <limits.h> header file. */
 #define HAVE_LIMITS_H 1
@@ -107,7 +77,7 @@
 #define HAVE_LOCALTIME 1
 
 /* Define to 1 if you have the <lzma.h> header file. */
-/* #define HAVE_LZMA_H 1 */
+/* #undef HAVE_LZMA_H */
 
 /* Define to 1 if you have the <malloc.h> header file. */
 /* #define HAVE_MALLOC_H */
@@ -126,11 +96,8 @@
 
 /* mmap() is no good without munmap() */
 #if defined(HAVE_MMAP) && !defined(HAVE_MUNMAP)
-#  undef /**/ HAVE_MMAP
+#undef /**/ HAVE_MMAP
 #endif
-
-/* Define to 1 if you have the <nan.h> header file. */
-/* #undef HAVE_NAN_H */
 
 /* Define to 1 if you have the <ndir.h> header file, and it defines `DIR'. */
 /* #undef HAVE_NDIR_H */
@@ -202,12 +169,6 @@
 /* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
 
-/* Define to 1 if you have the `strdup' function. */
-#define HAVE_STRDUP 1
-
-/* Define to 1 if you have the `strerror' function. */
-#define HAVE_STRERROR 1
-
 /* Define to 1 if you have the `strftime' function. */
 #define HAVE_STRFTIME 1
 
@@ -216,9 +177,6 @@
 
 /* Define to 1 if you have the <string.h> header file. */
 #define HAVE_STRING_H 1
-
-/* Define to 1 if you have the `strndup' function. */
-#define HAVE_STRNDUP 1
 
 /* Define to 1 if you have the <sys/dir.h> header file, and it defines `DIR'.
    */
@@ -273,10 +231,7 @@
 #define HAVE_VSPRINTF 1
 
 /* Define to 1 if you have the <zlib.h> header file. */
-/* #define HAVE_ZLIB_H 1 */
-
-/* Define to 1 if you have the `_stat' function. */
-/* #undef HAVE__STAT */
+/* #undef HAVE_ZLIB_H */
 
 /* Whether __va_copy() is available */
 /* #undef HAVE___VA_COPY */
@@ -294,19 +249,19 @@
 #define PACKAGE_BUGREPORT ""
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "libxml2"
+#define PACKAGE_NAME ""
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libxml2 2.9.3"
+#define PACKAGE_STRING ""
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "libxml2"
+#define PACKAGE_TARNAME ""
 
 /* Define to the home page for this package. */
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.9.3"
+#define PACKAGE_VERSION ""
 
 /* Type cast for the send() function 2nd arg */
 #define SEND_ARG2_CAST /**/
@@ -321,7 +276,7 @@
 #define VA_LIST_IS_ARRAY 1
 
 /* Version number of package */
-#define VERSION "2.9.3"
+#define VERSION "2.9.9"
 
 /* Determine what socket length (socklen_t) data type is */
 #define XML_SOCKLEN_T socklen_t
@@ -330,9 +285,6 @@
    <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
    #define below would cause a syntax error. */
 /* #undef _UINT32_T */
-
-/* Using the Win32 Socket implementation */
-/* #undef _WINSOCKAPI_ */
 
 /* ss_family is not defined here, use __ss_family instead */
 /* #undef ss_family */
