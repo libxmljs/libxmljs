b123d6428630629161e505fe98009e04502fded8
rchipka <rchipka01@gmail.com>
2023-03-30 17:06:12 -0400

    Add log

--- scripts/init.js
+++ scripts/init.js
@@ -2,9 +2,11 @@ const fs = require("fs");
 const { execSync } = require("child_process");
 
 if (!fs.existsSync(__dirname + "/../vendor/libxml2")) {
+    console.log("Initializing submodules");
     execSync("npm run init-submodules", { stdio: "inherit" });
 }
 
 if (!fs.existsSync(__dirname + "/../src/libxml2.cc")) {
+    console.log("Running SWIG");
     execSync("npm run swig", { stdio: "inherit" });
 }
