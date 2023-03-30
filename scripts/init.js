7ecce164695113cced4f7e65aa7b03cae9adfdf5
rchipka <rchipka01@gmail.com>
2023-03-30 18:16:32 -0400

    static names

--- scripts/init.js
+++ scripts/init.js
@@ -1,4 +1,3 @@
-const package = require(__dirname + "/../package.json");
 const fs = require("fs");
 const { execSync } = require("child_process");
 
@@ -7,10 +6,10 @@ if (
     !fs.existsSync(__dirname + "/../vendor/libxml2/globals.c")
 ) {
     console.log("Initializing submodules");
-    execSync(package.scripts["init-submodules"], { stdio: "inherit" });
+    execSync("npm run init-submodules", { stdio: "inherit" });
 }
 
 if (!fs.existsSync(__dirname + "/../src/libxml2.cc")) {
     console.log("Running SWIG");
-    execSync(package.scripts.swig, { stdio: "inherit" });
+    execSync("npm run swig", { stdio: "inherit" });
 }
