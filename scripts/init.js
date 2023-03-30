515fa59ea6d371f0a3a806891908a8c076e19dd9
rchipka <rchipka01@gmail.com>
2023-03-30 17:59:12 -0400

    Try running git directly

--- scripts/init.js
+++ scripts/init.js
@@ -1,19 +1,16 @@
+const package = require(__dirname + "/../package.json");
 const fs = require("fs");
 const { execSync } = require("child_process");
-
-console.log("Initializing");
 
 if (
     !fs.existsSync(__dirname + "/../vendor/libxml2/include/libxml/parserInternals.h") ||
     !fs.existsSync(__dirname + "/../vendor/libxml2/globals.c")
 ) {
     console.log("Initializing submodules");
-    execSync("npm run init-submodules", { stdio: "inherit" });
+    execSync(package.scripts["init-submodules"], { stdio: "inherit" });
 }
 
 if (!fs.existsSync(__dirname + "/../src/libxml2.cc")) {
     console.log("Running SWIG");
-    execSync("npm run swig", { stdio: "inherit" });
+    execSync(package.scripts.swig, { stdio: "inherit" });
 }
-
-process.exit(1);
