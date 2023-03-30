889fcaa9f360dade1a1c4cd934ce719d40a32d33
rchipka <rchipka01@gmail.com>
2023-03-30 17:16:44 -0400

    Check multiple files

--- scripts/init.js
+++ scripts/init.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
 const { execSync } = require("child_process");
 
-if (!fs.existsSync(__dirname + "/../vendor/libxml2/include/libxml/parserInternals.h")) {
+if (
+    !fs.existsSync(__dirname + "/../vendor/libxml2/include/libxml/parserInternals.h") ||
+    !fs.existsSync(__dirname + "/../vendor/libxml2/globals.c")
+) {
     console.log("Initializing submodules");
     execSync("npm run init-submodules", { stdio: "inherit" });
 }
