091bf5d83193ef10fa3803d3e925df6d2f3bfdff
rchipka <rchipka01@gmail.com>
2023-03-31 11:33:43 -0400

    execSync cwd packageroot

--- scripts/init.js
+++ scripts/init.js
@@ -1,15 +1,16 @@
 const fs = require("fs");
 const { execSync } = require("child_process");
+const packageRoot = __dirname + "/../";
 
 if (
-    !fs.existsSync(__dirname + "/../vendor/libxml2/include/libxml/parserInternals.h") ||
-    !fs.existsSync(__dirname + "/../vendor/libxml2/globals.c")
+    !fs.existsSync(packageRoot + "vendor/libxml2/include/libxml/parserInternals.h") ||
+    !fs.existsSync(packageRoot + "vendor/libxml2/globals.c")
 ) {
     console.log("Initializing submodules");
-    execSync("npm run init-submodules", { stdio: "inherit" });
+    execSync("npm run init-submodules", { stdio: "inherit", cwd: packageRoot });
 }
 
 if (!fs.existsSync(__dirname + "/../src/libxml2.cc")) {
     console.log("Running SWIG");
-    execSync("npm run swig", { stdio: "inherit" });
+    execSync("npm run swig", { stdio: "inherit", cwd: packageRoot });
 }
