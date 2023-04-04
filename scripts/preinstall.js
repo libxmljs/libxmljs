03a5395f2dadac74f239468bb3fe23fb7a2e2bdc
rchipka <rchipka01@gmail.com>
2023-04-04 10:41:06 -0400

    use older linux (#620)
    * attempty
    
    * only init if git repo
    
    * print glibc version
    
    * Use older linux

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -3,8 +3,9 @@ const { execSync } = require("child_process");
 const packageRoot = __dirname + "/../";
 
 if (
-    !fs.existsSync(packageRoot + "vendor/libxml2/include/libxml/parserInternals.h") ||
-    !fs.existsSync(packageRoot + "vendor/libxml2/globals.c")
+    fs.existsSync(packageRoot + ".git") &&
+    (!fs.existsSync(packageRoot + "vendor/libxml2/include/libxml/parserInternals.h") ||
+        !fs.existsSync(packageRoot + "vendor/libxml2/globals.c"))
 ) {
     console.log("Initializing submodules");
     execSync("npm run init-submodules", { stdio: "inherit", cwd: packageRoot });
