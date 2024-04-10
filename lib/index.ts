a83de4336dfd3e8f76515bd8aeec95cba58dce73
Corentin Cheminaud <corentin.cheminaud@icloud.com>
2024-04-11 00:26:17 +0200

    fix: add missing XMLSchema export

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
 export { XMLDocument, HTMLDocument } from "./document";
 export { XMLNode, XMLElement, XMLAttribute, XMLNamespace } from "./node";
+export { XMLSchema } from "./schema";
 
 export * from "./api";
 export * from "./types";
